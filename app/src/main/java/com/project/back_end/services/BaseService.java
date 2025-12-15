8680cf12280594d53633eef945fc900b3fa98037
Patrick <patrickwarley@gmail.com>
2025-12-15 08:17:51 -0300

    feat:TokenService implemented

--- app/src/main/java/com/project/back_end/services/BaseService.java
+++ app/src/main/java/com/project/back_end/services/BaseService.java
@@ -18,125 +18,136 @@ import com.project.back_end.repo.PatientRepository;
 
 @Service
 public class BaseService {
-    private final TokenService tokenService;
-    private final AdminRepository adminRepository;
-    private final DoctorService doctorService;
-    private final DoctorRepository doctorRepository;
-    private final PatientRepository patientRepository;
-    private final PatientService patientService;
-    private final AppointmentRepository appointmentRepository;
+  private final TokenService tokenService;
+  private final AdminRepository adminRepository;
+  private final DoctorService doctorService;
+  private final DoctorRepository doctorRepository;
+  private final PatientRepository patientRepository;
+  private final PatientService patientService;
+  private final AppointmentRepository appointmentRepository;
 
-    @Autowired
-    public BaseService(TokenService tokenService, AdminRepository adminRepository, DoctorService doctorService,
-            DoctorRepository doctorRepository, PatientRepository patientRepository, PatientService patientService,
-            AppointmentRepository appointmentRepository, String internalErrorMessage) {
-        this.tokenService = tokenService;
-        this.adminRepository = adminRepository;
-        this.doctorService = doctorService;
-        this.doctorRepository = doctorRepository;
-        this.patientRepository = patientRepository;
-        this.patientService = patientService;
-        this.appointmentRepository = appointmentRepository;
-        this.internalErrorMessage = internalErrorMessage;
+  @Autowired
+  public BaseService(TokenService tokenService, AdminRepository adminRepository, DoctorService doctorService,
+      DoctorRepository doctorRepository, PatientRepository patientRepository, PatientService patientService,
+      AppointmentRepository appointmentRepository, String internalErrorMessage) {
+    this.tokenService = tokenService;
+    this.adminRepository = adminRepository;
+    this.doctorService = doctorService;
+    this.doctorRepository = doctorRepository;
+    this.patientRepository = patientRepository;
+    this.patientService = patientService;
+    this.appointmentRepository = appointmentRepository;
+    this.internalErrorMessage = internalErrorMessage;
+  }
+
+  private String internalErrorMessage = "An error ocurred. Please try again later!";
+
+  public ResponseEntity<Map<String, String>> validateToken(String token, String user) {
+    try {
+      if (tokenService.validateToken(user, token))
+        return ResponseEntity.ok().body(Map.of("message", "Token is valid!"));
+
+      return ResponseEntity.status(401).body(Map.of("message", "Invalid token!"));
+    } catch (Exception e) {
+      System.out.println("Error when validating token! - " + e.getMessage());
+      return ResponseEntity.internalServerError().body(Map.of("message", internalErrorMessage));
+    }
+  }
+
+  public ResponseEntity<Map<String, String>> validateAdmin(Admin receivedAdmin) {
+    try {
+      Admin admin = adminRepository.findByUsername(receivedAdmin.getUsername());
+
+      if (admin != null && receivedAdmin.getPassword().equals(admin.getPassword())) {
+        return ResponseEntity.ok()
+            .body(Map.of("message", "Successful Login!", "token",
+                tokenService.generateToken(receivedAdmin.getUsername(), "admin")));
+      }
+
+      return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials!"));
+
+    } catch (Exception e) {
+      System.out.println("Error when validating admin! - " + e.getMessage());
+      return ResponseEntity.internalServerError().body(Map.of("message", internalErrorMessage));
+    }
+  }
+
+  public Map<String, Object> filterDoctor(String name, String specialty, String time) {
+    if (name == null)
+      name = "";
+
+    if (!specialty.equals("") && specialty != null) {
+      if (time.equals("") && time != null)
+        return Map.of("doctors", doctorService.findDoctorsByNameSpecialtyandTime(name, specialty, time));
+    } else {
+      if (time.equals("") && time != null)
+        return Map.of("doctors", doctorService.filterDoctorByNameAndTime(name, time));
     }
 
-    private String internalErrorMessage = "An error ocurred. Please try again later!";
+    return Map.of("doctors", doctorService.geDoctors());
+  }
 
-    public ResponseEntity<Map<String, String>> validateToken(String token, String user){
-        try {
-            if(tokenService.validateToken(user, token)) return ResponseEntity.ok().body(Map.of("message", "Token is valid!"));
+  public int validateAppointment(Appointment aAppointment) {
+    try {
+      Optional<Doctor> doc = doctorRepository.findById(aAppointment.getDoctor().getId());
 
-            return ResponseEntity.status(401).body(Map.of("message", "Invalid token!"));
-        } catch (Exception e) {
-            System.out.println("Error when validating token! - "+e.getMessage());
-            return ResponseEntity.internalServerError().body(Map.of("message", internalErrorMessage));
-        }
+      if (!doc.isPresent())
+        return -1;
+
+      return doc.get().getAvailableTimes()
+          .stream()
+          .anyMatch(time -> time.contains("" + aAppointment.getAppointmentTime().getHour())) ? 1 : 0;
+
+    } catch (Exception e) {
+      System.out.println("An error ocurred when trying to validate appointment!");
+      return -1;
+    }
+  }
+
+  public boolean validatePatient(Patient patient) {
+    try {
+      Patient dbPatient = patientRepository.findByEmailOrPhone(patient.getEmail(), patient.getPhone());
+
+      return dbPatient != null ? false : true;
+    } catch (Exception e) {
+      System.out.println("An error ocurred when trying to validate the patient" + e.getMessage());
+      return false;
+    }
+  }
+
+  public ResponseEntity<Map<String, String>> validatePatientLogin(Patient patient) {
+    try {
+      Patient dbPatient = patientRepository.findByEmail(patient.getEmail());
+
+      if (dbPatient != null && dbPatient.getPassword().equals(patient.getPassword())) {
+        return ResponseEntity.ok().body(Map.of("token", tokenService.generateToken(patient.getEmail(), "patient")));
+      }
+
+      return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials!"));
+
+    } catch (Exception e) {
+      System.out.println("Error when validating Patient! - " + e.getMessage());
+      return ResponseEntity.internalServerError().body(Map.of("message", internalErrorMessage));
+    }
+  }
+
+  public ResponseEntity<Map<String, Object>> filterPatient(String token, String condition, String doctorName) {
+
+    if (!tokenService.validateToken(token, "patient"))
+      return ResponseEntity.status(401).body(Map.of("message", "Invalid token"));
+
+    Patient patient = patientRepository.findByEmail(tokenService.extractEmail(token));
+
+    if (!doctorName.equals("") && doctorName != null) {
+      if (!condition.equals("") && condition != null)
+        return ResponseEntity.ok().body(
+            Map.of("appointments", patientService.filterByDoctorAndCondition(doctorName, condition, patient.getId())));
+      else
+        return ResponseEntity.ok()
+            .body(Map.of("appointments", patientService.filterByDoctor(doctorName, patient.getId())));
+
     }
 
-    public ResponseEntity<Map<String, String>> validateAdmin(Admin receivedAdmin){
-        try {
-            Admin admin = adminRepository.findByUsername(receivedAdmin.getUsername());
-
-            if(admin != null && receivedAdmin.getPassword().equals(admin.getPassword())){
-                    return ResponseEntity.ok().body(Map.of("token", tokenService.generateToken(receivedAdmin.getUsername())));
-            }
-
-            return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials!"));
-            
-        } catch (Exception e) {
-            System.out.println("Error when validating admin! - "+e.getMessage());
-            return ResponseEntity.internalServerError().body(Map.of("message", internalErrorMessage));
-        }
-    }
-
-    public Map<String, Object> filterDoctor(String name, String specialty, String time){
-        if(name == null) name ="";
-
-        if(!specialty.equals("") && specialty != null){
-            if(time.equals("") && time != null) return Map.of("doctors", doctorService.findDoctorsByNameSpecialtyandTime(name, specialty, time));
-        }else{
-            if(time.equals("") && time != null) return Map.of("doctors", doctorService.filterDoctorByNameAndTime(name, time));
-        }
-
-        return Map.of("doctors",doctorService.geDoctors());   
-    }
-
-    public int validateAppointment(Appointment aAppointment){
-        try {
-            Optional<Doctor> doc = doctorRepository.findById(aAppointment.getDoctor().getId());
-
-            if(!doc.isPresent()) return -1;
-
-            return doc.get().getAvailableTimes()
-                .stream()
-                .anyMatch(time -> time.contains(""+aAppointment.getAppointmentTime().getHour())) ?1:0;
-
-        } catch (Exception e) {
-            System.out.println("An error ocurred when trying to validate appointment!");
-            return -1;
-        }
-    }
-
-    public boolean validatePatient(Patient patient){
-        try {
-            Patient dbPatient = patientRepository.findByEmailOrPhone(patient.getEmail(), patient.getPhone());
-            
-            return dbPatient !=null? false : true;
-        } catch (Exception e) {
-            System.out.println("An error ocurred when trying to validate the patient"+e.getMessage());
-            return false;
-        }
-    }
-
-    public ResponseEntity<Map<String, String>> validatePatientLogin(Patient patient){
-        try {
-            Patient dbPatient = patientRepository.findByEmail(patient.getEmail());
-
-            if(dbPatient != null && dbPatient.getPassword().equals(patient.getPassword())){
-                    return ResponseEntity.ok().body(Map.of("token", tokenService.generateToken(patient.getEmail())));
-            }
-
-            return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials!"));
-            
-        } catch (Exception e) {
-            System.out.println("Error when validating Patient! - "+e.getMessage());
-            return ResponseEntity.internalServerError().body(Map.of("message", internalErrorMessage));
-        }
-    }
-
-    public ResponseEntity<Map<String, Object>> filterPatient(String token, String condition, String doctorName){
-
-        if(!tokenService.validateToken(token, "patient")) return ResponseEntity.status(401).body(Map.of("message", "Invalid token"));
-
-        Patient patient = patientRepository.findByEmail(tokenService.extractEmail(token));
-
-        if(!doctorName.equals("") && doctorName != null){
-            if(!condition.equals("") && condition!= null)
-                return ResponseEntity.ok().body(Map.of("appointments", patientService.filterByDoctorAndCondition(doctorName, condition, patient.getId())));
-            else  return ResponseEntity.ok().body(Map.of("appointments", patientService.filterByDoctor(doctorName, patient.getId())));
-
-        }
-
-        return ResponseEntity.ok().body(Map.of("appointments", appointmentRepository.findByPatientId(patient.getId())));
-    }
+    return ResponseEntity.ok().body(Map.of("appointments", appointmentRepository.findByPatientId(patient.getId())));
+  }
 }
--- app/src/main/java/com/project/back_end/services/DoctorService.java
+++ app/src/main/java/com/project/back_end/services/DoctorService.java
@@ -21,187 +21,208 @@ import com.project.back_end.models.Doctor;
 import com.project.back_end.repo.AppointmentRepository;
 import com.project.back_end.repo.DoctorRepository;
 
-
 @Service
 public class DoctorService {
 
-    private final DoctorRepository doctorRepository;
-    private final AppointmentRepository appointmentRepository;
-    private final TokenService tokenService;
+  private final DoctorRepository doctorRepository;
+  private final AppointmentRepository appointmentRepository;
+  private final TokenService tokenService;
 
-    @Autowired
-    public DoctorService(DoctorRepository doctorRepository, AppointmentRepository appointmentRepository,
-            TokenService tokenService) {
-        this.doctorRepository = doctorRepository;
-        this.appointmentRepository = appointmentRepository;
-        this.tokenService = tokenService;
+  @Autowired
+  public DoctorService(DoctorRepository doctorRepository, AppointmentRepository appointmentRepository,
+      TokenService tokenService) {
+    this.doctorRepository = doctorRepository;
+    this.appointmentRepository = appointmentRepository;
+    this.tokenService = tokenService;
+  }
+
+  // 4. **getDoctorAvailability Method**:
+  // - Retrieves the available time slots for a specific doctor on a particular
+  // date and filters out already booked slots.
+  // - The method fetches all appointments for the doctor on the given date and
+  // calculates the availability by comparing against booked slots.
+  // - Instruction: Ensure that the time slots are properly formatted and the
+  // available slots are correctly filtered.
+  @Transactional
+  public List<String> getDoctorAvailability(long doctorId, LocalDate date) {
+    Optional<Doctor> doc = doctorRepository.findById(doctorId);
+
+    if (!doc.isPresent())
+      return null;
+
+    Doctor doctor = doc.get();
+    List<Appointment> appointments = appointmentRepository.findByDoctorIdAndAppointmentTimeBetween(doctorId,
+        date.atStartOfDay(), date.atTime(23, 59));
+
+    if (appointments.isEmpty())
+      return doctor.getAvailableTimes();
+
+    return doctor.getAvailableTimes().stream().filter(time -> {
+      for (Appointment appointment : appointments) {
+        LocalDateTime start = appointment.getAppointmentTime();
+        String aux = "" + start.getHour() + ":" + start.getMinute() + "-" + (start.getHour() + 1) + ":"
+            + start.getMinute();
+
+        if (time.equals(aux))
+          return false;
+      }
+
+      return true;
+    }).collect(Collectors.toList());
+  }
+
+  @Transactional
+  public int saveDoctor(Doctor doctor) {
+    try {
+      Doctor sameEmailDoc = doctorRepository.findByEmail(doctor.getEmail());
+      if (sameEmailDoc != null)
+        return -1;
+
+      doctorRepository.save(doctor);
+      return 1;
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return 0;
     }
+  }
 
-// 4. **getDoctorAvailability Method**:
-//    - Retrieves the available time slots for a specific doctor on a particular date and filters out already booked slots.
-//    - The method fetches all appointments for the doctor on the given date and calculates the availability by comparing against booked slots.
-//    - Instruction: Ensure that the time slots are properly formatted and the available slots are correctly filtered.
-    @Transactional
-    public List<String> getDoctorAvailability(long doctorId, LocalDate date){
-        Optional<Doctor> doc = doctorRepository.findById(doctorId);
+  @Transactional
+  public int updateDoctor(Doctor doctor) {
+    try {
+      Optional<Doctor> doctorDb = doctorRepository.findById(doctor.getId());
 
-        if(!doc.isPresent()) return null;
+      if (!doctorDb.isPresent())
+        return -1;
 
-        Doctor doctor = doc.get();
-        List<Appointment> appointments = appointmentRepository.findByDoctorIdAndAppointmentTimeBetween(doctorId,date.atStartOfDay(),date.atTime(23, 59));
+      doctorRepository.save(doctor);
 
-        if(appointments.isEmpty()) return doctor.getAvailableTimes();
-
-        return doctor.getAvailableTimes().stream().filter(time -> {
-            for(Appointment appointment: appointments){
-                LocalDateTime start= appointment.getAppointmentTime();
-                String aux = ""+start.getHour()+":"+start.getMinute()+"-"+(start.getHour()+1)+":"+start.getMinute();
-
-                if(time.equals(aux)) return false;
-            }
-
-            return true;
-        }).collect(Collectors.toList());
-    } 
-    
-    @Transactional
-    public int saveDoctor(Doctor doctor){
-       try {
-           Doctor sameEmailDoc = doctorRepository.findByEmail(doctor.getEmail());  
-           if(sameEmailDoc != null) return -1;
-
-           doctorRepository.save(doctor);
-           return 1;
-       } catch (Exception e) {
-        System.out.println(e.getMessage());
-        return 0;
-       }
+      return 1;
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return 0;
     }
+  }
 
-    @Transactional
-    public int updateDoctor(Doctor doctor){
-        try {
-            Optional<Doctor> doctorDb = doctorRepository.findById(doctor.getId());
-
-            if(!doctorDb.isPresent()) return -1;
-
-            doctorRepository.save(doctor);
-
-            return 1;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return 0;
-        }
+  @Transactional
+  public List<Doctor> geDoctors() {
+    try {
+      return doctorRepository.findAll();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
     }
+  }
 
-    @Transactional
-    public List<Doctor> geDoctors(){
-        try {
-            return doctorRepository.findAll();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+  @Transactional
+  public int deleteDoctor(long doctorId) {
+    try {
+      Optional<Doctor> docToDelete = doctorRepository.findById(doctorId);
+
+      if (!docToDelete.isPresent())
+        return -1;
+
+      doctorRepository.delete(docToDelete.get());
+      appointmentRepository.deleteAllByDoctorId(doctorId);
+      return 1;
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return 0;
     }
+  }
 
-    @Transactional
-    public int deleteDoctor(long doctorId){
-        try {
-           Optional<Doctor> docToDelete = doctorRepository.findById(doctorId);
+  public ResponseEntity<Map<String, Object>> validateDoctor(Login login) {
+    try {
+      Doctor doctor = doctorRepository.findByEmail(login.getIdentifier());
+      Map<String, Object> invalid = Map.of("error", true, "token", null, "message", "Invalid credentials!");
 
-           if(!docToDelete.isPresent()) return -1;
+      if (doctor == null)
+        return ResponseEntity.badRequest().body(invalid);
 
-           doctorRepository.delete(docToDelete.get());
-           appointmentRepository.deleteAllByDoctorId(doctorId);
-           return 1;
+      if (!login.getPassword().equals(doctor.getPassword()))
+        return ResponseEntity.badRequest().body(invalid);
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return 0;
-        }
+      String token = tokenService.generateToken(doctor.getEmail(), "doctor");
+      return ResponseEntity.ok().body(Map.of("error", false, "token", token));
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return ResponseEntity.badRequest()
+          .body(Map.of("error", true, "token", null, "message", "An internal error ocurred. Try again later!"));
     }
+  }
 
-    public ResponseEntity<Map<String, Object>> validateDoctor(Login login){
-        try {
-            Doctor doctor = doctorRepository.findByEmail(login.getIdentifier());
-            Map<String, Object> invalid= Map.of("error",true, "token", null, "message", "Invalid credentials!");
-
-            if(doctor == null) return ResponseEntity.badRequest().body(invalid);
-
-            if(!login.getPassword().equals(doctor.getPassword())) return ResponseEntity.badRequest().body(invalid);
-            
-            String token =tokenService.generateToken(doctor.getEmail());
-            return ResponseEntity.ok().body(Map.of("error", false, "token", token));
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return ResponseEntity.badRequest().body(Map.of("error", true, "token", null, "message","An internal error ocurred. Try again later!"));
-        }
+  @Transactional
+  public List<Doctor> findDoctorByName(String name) {
+    try {
+      return doctorRepository.findByNameLike(name);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return null;
     }
+  }
 
-    @Transactional
-    public List<Doctor> findDoctorByName(String name){
-        try {
-            return doctorRepository.findByNameLike(name);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+  public Map<String, Object> findDoctorsByNameSpecialtyandTime(String name, String specialty, String amPm) {
+    try {
+      List<Doctor> doctors = doctorRepository.findByNameContainingIgnoreCaseAndSpecialtyIgnoreCase(name, specialty);
+
+      return Map.of("doctors",
+          doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm)))
+              .collect(Collectors.toList()));
+
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Map.of("error", null, "message", "An error ocurred try again later!");
     }
+  }
 
-    public Map<String, Object> findDoctorsByNameSpecialtyandTime(String name, String specialty, String amPm){
-        try {
-            List<Doctor> doctors = doctorRepository.findByNameContainingIgnoreCaseAndSpecialtyIgnoreCase(name, specialty);
-
-            return Map.of("doctors", doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm))).collect(Collectors.toList()));
-            
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return Map.of("error", null, "message","An error ocurred try again later!");
-        }
+  public Map<String, Object> filterDoctorByTime(String amPm) {
+    try {
+      List<Doctor> doctors = doctorRepository.findAll();
+      return Map.of("doctors",
+          doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm)))
+              .collect(Collectors.toList()));
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Map.of("error", null, "message", "An error ocurred try again later!");
     }
+  }
 
-    public Map<String, Object> filterDoctorByTime (String amPm){
-        try {
-            List<Doctor> doctors = doctorRepository.findAll();
-            return Map.of("doctors", doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm))).collect(Collectors.toList()));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return Map.of("error", null, "message","An error ocurred try again later!");
-        }
+  public Map<String, Object> filterDoctorByNameAndTime(String name, String amPm) {
+    List<Doctor> doctors = doctorRepository.findByNameLike(name);
+
+    return Map.of("doctors",
+        doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm)))
+            .collect(Collectors.toList()));
+  }
+
+  public Map<String, Object> filterDoctorByNameAndSpeciality(String name, String specialty) {
+    try {
+      return Map.of("Doctors", doctorRepository.findByNameContainingIgnoreCaseAndSpecialtyIgnoreCase(name, specialty));
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Map.of("error", null, "message", "An error ocurred try again later!");
     }
+  }
 
-    public Map<String, Object> filterDoctorByNameAndTime(String name, String amPm){
-        List<Doctor> doctors = doctorRepository.findByNameLike(name);
-
-        return Map.of("doctors", doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time ->time.contains(amPm))).collect(Collectors.toList()));
+  public Map<String, Object> filterDoctorByTimeAndSpecialty(String specialty, String amPm) {
+    try {
+      List<Doctor> doctors = doctorRepository.findAll();
+      return Map.of("doctors",
+          doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm)))
+              .collect(Collectors.toList()));
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Map.of("error", null, "message", "An error ocurred try again later!");
     }
+  }
 
-    public Map<String, Object> filterDoctorByNameAndSpeciality(String name, String specialty){
-        try {
-            return Map.of("Doctors", doctorRepository.findByNameContainingIgnoreCaseAndSpecialtyIgnoreCase(name, specialty));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return Map.of("error", null, "message","An error ocurred try again later!");
-        }
+  public Map<String, Object> filterDoctorBySpecialty(String specialty) {
+    try {
+      return Map.of("Doctors", doctorRepository.findBySpecialtyIgnoreCase(specialty));
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Map.of("error", null, "message", "An error ocurred try again later!");
     }
-
-    public Map<String, Object> filterDoctorByTimeAndSpecialty(String specialty, String amPm){
-        try {
-            List<Doctor> doctors = doctorRepository.findAll();
-            return Map.of("doctors", doctors.stream().filter(doc -> doc.getAvailableTimes().stream().anyMatch(time -> time.contains(amPm))).collect(Collectors.toList()));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return Map.of("error", null, "message","An error ocurred try again later!");
-        }
-    }
-
-    public Map<String, Object> filterDoctorBySpecialty(String specialty){
-        try {
-            return Map.of("Doctors", doctorRepository.findBySpecialtyIgnoreCase(specialty));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return Map.of("error", null, "message","An error ocurred try again later!");
-        }
-    }  
+  }
 }
